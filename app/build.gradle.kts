4c1d938b0dab5fd32a467f94f4e34cef3de12454
J0nny-G <joaopedrogoncalves@ipvc.pt>
2024-06-19 13:25:11 +0100

    Profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp.logging.interceptor)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
