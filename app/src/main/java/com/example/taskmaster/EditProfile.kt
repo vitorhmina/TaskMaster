eb9fd07c77212167d9f906a2a796706e9f68b1ac
J0nny-G <joaopedrogoncalves@ipvc.pt>
2024-06-19 15:46:01 +0100

    Edit Profile

--- app/src/main/java/com/example/taskmaster/EditProfile.kt
+++ app/src/main/java/com/example/taskmaster/EditProfile.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class Users : AppCompatActivity() {
+class EditProfile  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.edit_profile)
     }
 }
--- app/src/main/java/com/example/taskmaster/Profile.kt
+++ app/src/main/java/com/example/taskmaster/Profile.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class Users : AppCompatActivity() {
+class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.profile)
     }
 }
